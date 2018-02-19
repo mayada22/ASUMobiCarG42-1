f99c1b05fafa08c4555fae8f7c0dc7684a1852aa
Ayaosama21 <36484232+Ayaosama21@users.noreply.github.com>
2018-02-19 22:52:19 +0200

    Add files via upload

---
+++ sheet1_q1/sheet1_q1.ino
@@ -0,0 +1,16 @@
+const int LED = 13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED,OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(LED,HIGH);   //to turn on led
+//time on=2000*0.25
+delay(500);
+digitalWrite(LED,LOW);    //to turn off led
+//time off=2000*0.75
+delay(1500);
+}
+
+
