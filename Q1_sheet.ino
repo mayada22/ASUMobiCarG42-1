28cddedf60dbcee0d350e126fb4615efeb51bc61
MohamedYaman <36310797+MohamedYaman@users.noreply.github.com>
2018-02-19 20:12:22 +0200

    Add files via upload

---
+++ Q1_sheet.ino
@@ -0,0 +1,13 @@
+int pinLED = 9 ; // the pin number I want to use 
+float Time = 2000 ; // total time  
+
+void setup() {
+  pinMode ( pinLED , OUTPUT ) ; // Declare pin to be Output
+}
+
+void loop() {
+digitalWrite ( pinLED , 1 ) ; // turn on for 0.25*Time " Duty cycle of 25%"
+delay (0.25 * Time ) ;
+digitalWrite ( pinLED , 0 ) ;
+delay (0.75 * Time ) ;
+}
