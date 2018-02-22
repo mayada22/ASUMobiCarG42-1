8d2ad2fb876fbc5e471fe19713b225bfd11dcc28
Kholeif <36528209+Kholeif@users.noreply.github.com>
2018-02-22 06:14:42 +0200

    Add files via upload

---
+++ Q2/Q2.ino
@@ -0,0 +1,26 @@
+void setup() 
+{
+  pinMode(4 , OUTPUT);
+  pinMode(5 , OUTPUT);
+  pinMode(6 , OUTPUT);
+  pinMode(7 , OUTPUT);
+}
+
+void loop() 
+{
+  for (int i=4;i<8;i++)
+  {
+    digitalWrite (i,HIGH);
+    delay(1000);
+    digitalWrite (i,LOW);
+    delay(1000);
+  }
+  for (int j=6;j>4;j--)
+  {
+    digitalWrite (j,HIGH);
+    delay(1000);
+    digitalWrite (j,LOW);
+    delay(1000);
+  }
+                        
+}
