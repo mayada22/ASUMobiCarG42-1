b46e570153f400ada1894a34ce294710b988bd84
mayada22 <36500546+mayada22@users.noreply.github.com>
2018-02-25 20:25:21 +0200

    Add files via upload

---
+++ example2/bush_button/bush_button.ino
@@ -0,0 +1,25 @@
+
+const int buttonPin = 2;     
+const int led =  13;      
+int val = 0;        
+
+void setup() {
+  
+  pinMode(led, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+ 
+  val = digitalRead(buttonPin);
+  if (val == HIGH) {
+  
+    digitalWrite(led, HIGH);
+    delay (1000);
+    digitalWrite(led , LOW);
+    delay(1000);
+  } 
+  else {
+    digitalWrite(led, LOW);
+  }
+}
