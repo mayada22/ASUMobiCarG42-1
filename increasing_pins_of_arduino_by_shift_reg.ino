2f7a5502d9f86e32fdc9dac23cda76c1ebcc36d5
Ayaosama21 <36484232+Ayaosama21@users.noreply.github.com>
2018-03-27 23:53:29 +0200

    Add files via upload

---
+++ increasing_pins_of_arduino_by_shift_reg.ino
@@ -0,0 +1,38 @@
+int DS_pin = 8;
+int STCP_pin = 9;
+int SHCP_pin = 10;
+
+void setup() {
+  pinMode(DS_pin,OUTPUT);
+  pinMode(STCP_pin,OUTPUT);
+  pinMode(SHCP_pin,OUTPUT);
+  writereg();
+}
+
+boolean registers[8];
+void writereg()
+{
+  digitalWrite(SHCP_pin,LOW);
+
+for(int i=7;i>=0;i--)       //i is the number of output
+{
+  digitalWrite(STCP_pin,LOW);
+  digitalWrite(DS_pin,registers[i]);
+  digitalWrite(STCP_pin,HIGH);
+}
+digitalWrite(SHCP_pin,HIGH);
+}
+void loop() {
+  for(int i = 0;i<9;i++)
+  {
+    registers[i]=HIGH;
+    delay(100);
+    writereg();
+  }
+  for(int i = 8;i>0;i++)
+  {
+    registers[i]=LOW;
+    delay(100);
+    writereg();
+  }
+}  
